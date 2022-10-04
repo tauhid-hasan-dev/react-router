45ecb4ce0b75258045d8835c6f79f0feb52089e0
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-04 19:47:04 +0300

    hero icon added to the user details

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -6,7 +6,7 @@ const User = ({user}) => {
     return (
         <div className='border border-5 p-5 shadow-md'>
             <h3 className='text-2xl'>Name: {name}</h3>
-            <h3 className='hover:bg-red-500' >Username: <Link to={`/users/${id}`} >{username}</Link></h3>
+            <h3 className='hover:bg-green-200 ' >Username: <Link to={`/users/${id}`} >{username}</Link></h3>
             <h3>Email: {email}</h3>
         </div>
     );
--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { BeakerIcon, GlobeAltIcon, DevicePhoneMobileIcon } from '@heroicons/react/24/solid'
 
 const UserDetails = () => {
     const user = useLoaderData();
@@ -7,10 +8,10 @@ const UserDetails = () => {
     const {street, suite, city , zipcode} = address;
     return (
         <div className='flex flex-col items-center p-10'>
-            <div className='border shadow-2xl p-20'>
+            <div className='border shadow-2xl p-20 flex flex-col gap-3'>
                 <h1 className='text-3xl font-bold'>{name}</h1>
-                <p>{phone}</p>
-                <p>{website}</p>
+                <p className='flex flex-row gap-3'><DevicePhoneMobileIcon className="h-6 w-6 text-slate-800"/>{phone}</p>
+                <p className='flex flex-row gap-3'><GlobeAltIcon className="h-6 w-6 text-slate-800"/>{website}</p>
                 <div className='mt-5'>
                     <p className='text-lg font-semibold'> Adress</p>
                     <p>City: {city}</p>
